f3c98d431ec20497fad79b7db9b0db10d66a288a
Aqua-Pura <103697225+guilhermevenancio27@users.noreply.github.com>
2026-06-22 19:50:45 -0300

    removed duplicate lines from 'booking.js'

--- server/api/controllers/booking.js
+++ server/api/controllers/booking.js
@@ -1,83 +1,74 @@
 var mongoose = require('mongoose');
+
 var Booking = mongoose.model('Booking');
 var Car = mongoose.model('Cars');
 
-
-module.exports.bookingRead = function(req, res) {
-    Booking.find(function (err, booking) {
-        if (err)
-            res.send(err);
-        res.json(booking);
-    });
-
-};
-
-module.exports.bookingsReadByEmail = function (req,res) {
-    Booking.find({email:req.params.email}, function (err, bookings) {
-        if (err)
-            res.send(err);
-        res.json(bookings);
-    });
-};
-
-module.exports.createBooking = function (req, res) {
-    const BookingData = {
-        email: req.body.email,
-        car: req.body.car,
-        startdate: req.body.startdate,
-        enddate: req.body.enddate,
-        pickuploc: req.body.pickuploc,
-        dropoffloc: req.body.dropoffloc,
-        totalprice: req.body.totalprice,
+function sendErrorOrJson(res, err, data) {
+    if (err) {
+        return res.send(err);
     }
 
-    var booking = new Booking(BookingData)
+    return res.json(data);
+}
+
+function buildBookingData(body) {
+    return {
+        email: body.email,
+        car: body.car,
+        startdate: body.startdate,
+        enddate: body.enddate,
+        pickuploc: body.pickuploc,
+        dropoffloc: body.dropoffloc,
+        totalprice: body.totalprice
+    };
+}
+
+function updateCarAvailability(carid, callback) {
+    Car.update(
+        { _id: carid },
+        {
+            $set: {
+                isavailable: false
+            }
+        },
+        callback
+    );
+}
+
+function saveBooking(res, booking) {
+    booking.save(function saveBookingCallback(err) {
+        if (err) {
+            return res.send(err);
+        }
+
+        return res.json({ message: 'Booking created' });
+    });
+}
+
+module.exports.bookingRead = function bookingRead(req, res) {
+    Booking.find(function findBookingsCallback(err, bookings) {
+        return sendErrorOrJson(res, err, bookings);
+    });
+};
+
+module.exports.bookingsReadByEmail = function bookingsReadByEmail(req, res) {
+    Booking.find(
+        { email: req.params.email },
+        function findBookingsByEmailCallback(err, bookings) {
+            return sendErrorOrJson(res, err, bookings);
+        }
+    );
+};
+
+module.exports.createBooking = function createBooking(req, res) {
+    var booking = new Booking(buildBookingData(req.body));
     var carid = req.body.carid;
 
+    updateCarAvailability(carid, function updateCarAvailabilityCallback(err) {
+        if (err) {
+            return res.send(err);
+        }
 
-    var mongoose = require('mongoose');
-    var Booking = mongoose.model('Booking');
-    var Car = mongoose.model('Cars');
-
-
-    module.exports.bookingRead = function(req, res) {
-        Booking.find(function (err, booking) {
-            if (err)
-                res.send(err);
-            res.json(booking);
-        });
-
-    };
-
-    module.exports.bookingsReadByEmail = function (req,res) {
-        Booking.find({email:req.params.email}, function (err, bookings) {
-            if (err)
-                res.send(err);
-            res.json(bookings);
-        });
-    };
-
-    module.exports.createBooking = function (req, res) {
-        var booking = new Booking(req.body);
-        var carid = req.body.carid;
-
-        Car.update({_id: carid}, {
-            $set:{
-                isavailable : false
-            }
-        }, function(err, affected, resp) {
-            if(err){
-                return res.send(err);
-            }
-        });
-
-        booking.save(function (err) {
-            if(err){
-                return res.send(err);
-            }
-
-            res.json({message: 'Booking created'})
-        })
-    };
-
-}
+        return saveBooking(res, booking);
+    });
+};
