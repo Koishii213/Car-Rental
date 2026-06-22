12c6910db7abc7edfec72347c7876f2f8c77616f
Gabrielsilva56 <112819016+Gabrielsilva56@users.noreply.github.com>
2026-06-22 19:53:59 -0300

    fix: admincontrolpanel.component.ts
    O membro 'getAll' nunca é reatribuído; marquei como `somente leitura`.

--- client/src/app/admincontrolpanel/admincontrolpanel.component.ts
+++ client/src/app/admincontrolpanel/admincontrolpanel.component.ts
@@ -15,35 +15,33 @@ import {observeOn} from "rxjs/operators";
 
 export class AdmincontrolpanelComponent implements OnInit {
 
-  @Output() private getAll = new EventEmitter();
-  @Input() seletedCar:Car;
+  @Output() private readonly getAll = new EventEmitter();
+  @Input() seletedCar: Car;
 
-  showDeleteDialog :boolean = false;
-  showUpdataDialog :boolean = false;
-  showAddDialog :boolean = false;
+  showDeleteDialog: boolean = false;
+  showUpdataDialog: boolean = false;
+  showAddDialog: boolean = false;
+
   // imageName:string;
-  imageUrl:string = '/assets/car-rental-logo.jpg';
-  fileToUpload:File;
-  formCarInfo:Car;
+  imageUrl: string = '/assets/car-rental-logo.jpg';
+  fileToUpload: File;
+  formCarInfo: Car;
 
-
-
-  constructor(private productService:ProductService, private http:HttpClient) {
+  constructor(private productService: ProductService, private http: HttpClient) {
   }
 
   ngOnInit() {
     this.seletedCar = null;
-    if(this.seletedCar != null){
+
+    if (this.seletedCar != null) {
       this.formCarInfo = this.seletedCar;
-    }
-    else{
+    } else {
       this.initCarForm();
     }
-
   }
 
-  initCarForm(){
-    this.formCarInfo  =  new Car(
+  initCarForm() {
+    this.formCarInfo = new Car(
       '',
       'Standard',
       2,
@@ -58,95 +56,72 @@ export class AdmincontrolpanelComponent implements OnInit {
     );
   }
 
-  confirmDelete(){
+  confirmDelete() {
     this.productService.deleteCarById(this.seletedCar._id).subscribe(
-      (data)=>{
-        // console.log(data);
+      (data) => {
         console.log('*********');
-        // this.getAllCarList();
-      },(err)=>{
+      },
+      (err) => {
         console.log(err);
       }
     );
+
     this.getAllCarList();
     this.showDeleteDialog = false;
-    console.log("confirm delete: "+ this.seletedCar._id+ this.seletedCar.name);
-
+    console.log("confirm delete: " + this.seletedCar._id + this.seletedCar.name);
   }
 
-  getAllCarList(){
+  getAllCarList() {
     this.getAll.emit();
     this.showDeleteDialog = false;
     this.showUpdataDialog = false;
     this.showAddDialog = false;
   }
 
-  closeDialog(){
+  closeDialog() {
     this.showDeleteDialog = false;
     this.showUpdataDialog = false;
     this.showAddDialog = false;
   }
 
   addBtnClicked() {
-
     this.initCarForm();
     this.showAddDialog = true;
-
   }
+
   editBtnClick() {
     this.initCarForm();
-    if(this.seletedCar != null){
+
+    if (this.seletedCar != null) {
       this.showUpdataDialog = true;
       this.formCarInfo = this.seletedCar;
     }
-
-
   }
 
   deleteBtnClick() {
-    if(this.seletedCar != null){
+    if (this.seletedCar != null) {
       this.showDeleteDialog = !this.showDeleteDialog;
     }
   }
-
-  // onFileSeleted(e) {
-  //   this.imageName = e.target.files[0];
-  //   alert(this.imageName);
-  //
-  // }
-  //
-  // onUpload() {
-  //   const fd = new FormData();
-  //   fd.append('image', this.imageName);
-  //   // this.http.post()
-  // }
 
   handleFileInput(file: FileList) {
     console.log('handlefile');
     this.fileToUpload = file.item(0);
 
     let reader = new FileReader();
-    reader.onload = (event:any) => {
+
+    reader.onload = (event: any) => {
       this.imageUrl = event.target.result;
     };
 
     reader.readAsDataURL(this.fileToUpload);
-    this.formCarInfo.imageName = '/assets/uploadedImage/'+this.fileToUpload.name;
-
-    //
-    // reader.onload = function(e) {
-    //   var url = e.target;
-    //   console.log(e.target); //返回图片url
-    // };
-
+    this.formCarInfo.imageName = '/assets/uploadedImage/' + this.fileToUpload.name;
   }
 
-  onImageSubmit(){
+  onImageSubmit() {
     console.log('onImageSubmit');
-    // this.http.post('/api/image', sdfs);
+
     let file = this.fileToUpload;
-    // const headers = new HttpHeaders()
-    //   .append('Content-Type', 'multipart/form-data');
 
     const formData: FormData = new FormData();
     formData.append('CarImage', file, file.name);
@@ -164,88 +139,47 @@ export class AdmincontrolpanelComponent implements OnInit {
         }
       );
 
-    // const endpoint = '/api/image/post';
-    // const formData: FormData = new FormData();
-    // // formData.append('fileKey', file, file.name);
-    // formData.set('imageName', this.fileToUpload.name);
-    // formData.set('imageData', this.imageUrl);
-    //
-    //
-    //
-    // this.http
-    //   .post(endpoint, formData ).subscribe(
-    //   res => {
-    //           console.log("get response after post picture");
-    //           console.log(res);
-    //         },
-    //         err => {
-    //           console.log("Error occured when post Image");
-    //         }
-    // );
-
-
     console.log('postCar finish');
-
   }
 
-
   // setACsup($event) {
-  //   console.log($event.target.value);
-  //   console.log(this.formCarInfo.ACsup);
-  //   // if($event.target.value == 'true'){
-  //   //   this.formCarInfo.ACsup = true;
-  //   //
-  //   //   // formCarInfo_ACsup="";
-  //   //   console.log('setacsup to true');
-  //   // }
-  //   // else {
-  //   //   this.formCarInfo.ACsup = false;
-  //   //   console.log('setacsup to false');
-  //   // }
-  //   console.log($event.target.value);
-  //   console.log(this.formCarInfo.ACsup);
-  //
   // }
 
   // setIsAuto($event) {
-  //   console.log($event.target.value);
-  //   console.log(this.formCarInfo.isAuto);
-  //   if($event.target.value == 'YES'){
-  //     this.formCarInfo.isAuto = true;
-  //     // formCarInfo_isAuto: any;
-  //   }
-  //   else{
-  //     this.formCarInfo.isAuto = false;
-  //   }
-  //   console.log($event.target.value);
-  //   console.log(this.formCarInfo.isAuto);
-  //
-  //
   // }
+
   clickToAddCar() {
     this.showAddDialog = false;
+
     console.log(this.formCarInfo);
+
     this.productService.createCar(this.formCarInfo).subscribe(
-      (data)=>{
+      (data) => {
         console.log(data);
-      },(err)=>{
+      },
+      (err) => {
         console.log(err);
       }
     );
+
     this.getAllCarList();
   }
 
   confirmUpdateCarInfo() {
     this.showUpdataDialog = false;
+
     console.log('---updated infor---');
     console.log(this.formCarInfo);
+
     this.productService.putCar(this.formCarInfo).subscribe(
-      (data)=>{
+      (data) => {
         console.log(data);
-      },(err)=>{
+      },
+      (err) => {
         console.log(err);
       }
     );
+
     this.getAllCarList();
   }
 }
