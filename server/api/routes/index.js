db09a4f0a56a56f9f2c275394c19ebfe32983c7b
Aqua-Pura <103697225+guilhermevenancio27@users.noreply.github.com>
2026-06-23 00:16:48 -0300

    atualizacao 'index.js'

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('express-jwt');
+
 var auth = jwt({
     secret: 'MY_SECRET',
     userProperty: 'payload'
@@ -22,36 +23,49 @@ router.get('/profile', auth, ctrlProfile.profileRead);
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
 
-//router.get('/account',ctrlacc.accountRead);
-router.get('/account/:email',ctrlacc.accountReadByEmail);
-router.post('/account',ctrlacc.createaccount);
-router.post('/account/:email',ctrlacc.updateaccountByEmail);
-// routers for car CRUD service;
+// account
+// router.get('/account', ctrlacc.accountRead);
+router.get('/account/:email', ctrlacc.accountReadByEmail);
+router.post('/account', ctrlacc.createaccount);
+router.post('/account/:email', ctrlacc.updateaccountByEmail);
+
+// car CRUD service
 router.get('/carlists', carList.readAllCarInfo);
 router.post('/carlists', carList.postCarInfor);
 
-router.get('/booking/:email',ctrlBooking.bookingsReadByEmail);
+// booking
+router.get('/booking/:email', ctrlBooking.bookingsReadByEmail);
 router.post('/booking', ctrlBooking.createBooking);
 
+// favorite list
 router.post('/favoritelist', ctrlfavoritelist.createFavorite);
 router.delete('/favoritelist/:email&:carid', ctrlfavoritelist.DeleteFavorite);
 router.get('/favoritelist/:email', ctrlfavoritelist.CarsReadByEmail);
+
+// create initial cars
 router.get('/carlists/post', carList.createCarContext);
 
-//create car
+// create car
 router.post('/car', carList.createCar);
-//delete a car
+
+// delete car
 router.delete('/car/:id', carList.deleteCarbyId);
+
 console.log('---/car start---');
+
+// update car
 router.put('/car', carList.updateCarInfo);
 
-//router for car id
+// search car by id
 router.get('/car/:id', carList.searchCarbyID);
 
-//search car by pick location
+// search car by pickup location / city
 router.get('/carlists/search/:pickupLoc', carList.searchCarProduct);
 
-//search car by filter condition
+// new route for location feature
+router.get('/carlists/location/:pickupLoc', carList.searchCarProduct);
+
+// search car by filter condition
 router.get('/carlists/filter/:pickupLoc&:priceMax&:priceMin&:carType&:passNum', carList.searchCarwithFilter);
 
 module.exports = router;
